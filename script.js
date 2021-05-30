bb15226b46bfef252609c1ff74ad2a931beaced7
yugmani <yugmani@yahoo.com>
2021-05-29 18:27:22 -0700

    Difference of map and forEach

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const words = ['react', 'script', 'interview', 'style', 'javascript'];
 // using higher order function
 const ans = words.filter(word => word.length > 6);
 
-console.log(ans); //Expected ['interview', 'javascript']
+// console.log(ans); //Expected ['interview', 'javascript']
 
 //using for.. loop
 const newArray = [];
@@ -14,4 +14,39 @@ for (let i = 0; i < words.length; i++) {
   }
 }
 
-console.log(newArray);
+// console.log(newArray);
+
+// 2. Difference between map and forEach
+
+// map returns a new Array, forEach doesn't
+// method chaining can be done in map but not forEach
+// map and forEach don't mutate the original array
+
+// // Return a new array where even numbers are multiplied by 2
+let array1 = [1, 2, 3, 4, 5, 6, 7];
+
+// using map function
+function returnEven(arr) {
+  let data = arr.map(num => (num % 2 === 0 ? num * 2 : num * 1));
+  return data;
+}
+
+// console.log(returnEven(array1)); // Expected [1,  4, 3, 8, 5, 12, 7]
+
+//using forEach
+function consoleEven(arr) {
+  let data = arr.forEach(num => (num % 2 === 0 ? num * 2 : num * 1));
+  return data;
+}
+
+// console.log(consoleEven(array1)); // undefined;
+
+// // we are converting the new array back to original
+function returnOriginal(arr) {
+  let data = arr
+    .map(num => (num % 2 === 0 ? num * 2 : num * 1))
+    .map(item => (item % 2 === 0 ? item / 2 : item));
+  return data;
+}
+
+console.log(returnOriginal(array1)); // Expected [1, 2, 3, 4, 5, 6, 7];
